6da97e656ea24ec5f20507e0a04d4d14aa4e8526
Michel Wacker <mw@gentletroll.com>
2015-01-23 23:37:41 +0100

    add CameraFade

---
+++ Assets/Scripts/Camera/CameraFade.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFade : MonoBehaviour {
+	
+	public Animator anim;
+	
+	
+	void Awake () {
+		if(anim == null)
+			Debug.Log("No animator found in CameraFade");
+	}
+	
+	// Use this for initialization
+	void Start () {
+		iTween.CameraFadeAdd ();
+	}
+	
+	public void FadeOutCamera()
+	{
+		iTween.CameraFadeTo (iTween.Hash ("amount", 1f, "time", 1f, "oncomplete", "OnFadeComplete", "oncompletetarget", gameObject));
+		
+		anim.SetTrigger ("MenuFadeOut");
+	}
+	
+	private void OnFadeComplete()
+	{
+		Debug.Log("FadeComplete");
+	}
+}
