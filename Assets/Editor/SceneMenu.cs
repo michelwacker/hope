8d3b172ddc654d8538c0c850f2d9754f1b90956d
Michel Wacker <mw@gentletroll.com>
2015-01-23 23:15:37 +0100

    initial asset import

---
+++ Assets/Editor/SceneMenu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class SceneMenu: Editor
+{
+	[MenuItem("Open Scene/Scene1")]
+	public static void OpenS1()
+	{
+		OpenScene ("Scene1");
+	}
+	private static void OpenScene(string name)
+	{
+		if (EditorApplication.SaveCurrentSceneIfUserWantsTo())
+		{
+			EditorApplication.OpenScene("Assets/Scenes/" + name + ".unity");
+		}
+	}
+}
