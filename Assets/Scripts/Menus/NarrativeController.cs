a7e7e07e6e6d7f08631c1bf8f905331e4097f440
Michel Wacker <mw@gentletroll.com>
2015-01-24 21:37:53 +0100

    add NarrativeController

---
+++ Assets/Scripts/Menus/NarrativeController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NarrativeController : MonoBehaviour
+{
+	public static void Write(string text)
+	{
+		instance.StartTextOutput (text);
+	}
+	private static NarrativeController instance;
+
+	public Text output;
+	// Use this for initialization
+	void Awake ()
+	{
+		if (instance == null)
+		{
+			instance = this;
+		}
+	}
+
+	void StartTextOutput(string text)
+	{
+		// TODO start coroutine here instead
+		output.text = text;
+	}
+}
