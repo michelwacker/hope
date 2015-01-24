7d7d8db9e2e07c6f57cfe4cb280b4a0ce154a421
Michel Wacker <mw@gentletroll.com>
2015-01-24 17:13:14 +0100

    add manager for interaction menu

---
+++ Assets/Scripts/Menus/InteractionMenuManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InteractionMenuManager : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+
+	public void HideMenu()
+	{
+		gameObject.SetActive (false);
+	}
+
+	public void ShowMenu(Vector3 position)
+	{
+		gameObject.transform.position = new Vector3 (position.x, position.y, position.z);
+
+		gameObject.SetActive (true);
+	}
+}
