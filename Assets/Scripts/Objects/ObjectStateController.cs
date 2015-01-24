3b5c0710db0e9285771b6ff44922841aa75faa82
Michel Wacker <mw@gentletroll.com>
2015-01-24 21:09:06 +0100

    add ObjectStateController

---
+++ Assets/Scripts/Objects/ObjectStateController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectStateController : MonoBehaviour, IUseInteraction, IBreakInteraction
+{
+	private Animator anim;
+	void Awake()
+	{
+		anim = GetComponent<Animator> ();
+		if (anim == null)
+		{
+			Debug.LogError("No animator found in " + name + "! Must be added to set states on interaction.");
+		}
+	}
+	#region IBreakInteraction implementation
+
+	public void Break ()
+	{
+		if (anim != null)
+			anim.SetTrigger ("Break");
+	}
+
+	#endregion
+
+	#region IUseInteraction implementation
+
+	public void Use ()
+	{
+		if (anim != null)
+			anim.SetTrigger ("Use");
+	}
+
+	#endregion
+}
