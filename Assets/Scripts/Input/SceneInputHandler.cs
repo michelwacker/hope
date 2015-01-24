2b2d339eeea09dd1e57c3e1646a2bce78af775d6
Michel Wacker <mw@gentletroll.com>
2015-01-24 15:15:20 +0100

    add scene input handler

---
+++ Assets/Scripts/Input/SceneInputHandler.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneInputHandler : MonoBehaviour, IInputHandler
+{	
+	public Player player;
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+
+	#region IInputHandler implementation
+	public void HandleInputDown (Collider2D target, int fingerId = -1)
+	{
+		if (player != null)
+		{
+			Transform t = target.transform.FindChild("WalkAnchor");
+			if (t == null)
+				t = target.transform;
+			player.WalkToPosition(t.position);
+		}
+	}
+	public void HandleInputUpdate (Vector2 position, int fingerId = -1)
+	{
+		// empty
+	}
+	public void HandleInputUp (int fingerId = -1)
+	{
+		// empty
+	}
+	#endregion
+}
