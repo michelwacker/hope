c323dc6b7b404de2178e1761bffe63bad6467646
Michel Wacker <mw@gentletroll.com>
2015-01-24 18:56:12 +0100

    add craziness modifier script

---
+++ Assets/Scripts/Objects/CrazinessModifier.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrazinessModifier : MonoBehaviour, IUseInteraction, IBreakInteraction
+{
+	[Range(-3, 3)]
+	public int crazinessOnBreak = 0;
+	[Range(-3, 3)]
+	public int crazinessOnUse = 0;
+
+	#region IBreakInteraction implementation
+	public void Break ()
+	{
+		Debug.Log ("Break!");
+		// TODO add player craziness
+	}
+	#endregion
+
+	#region IUseInteraction implementation
+	public void Use ()
+	{
+		Debug.Log ("Use!");
+		// TODO add player craziness
+	}
+	#endregion
+}
