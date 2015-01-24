3573c69396a73a7c65739f740162566bd4739d57
Michel Wacker <mw@gentletroll.com>
2015-01-24 21:53:43 +0100

    add layer sorter

---
+++ Assets/Scripts/Scene/LayerSorter.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LayerSorter : MonoBehaviour
+{
+	void Start ()
+	{
+		// auto sort all renderers as they were found in the hierarchy
+		SpriteRenderer[] renderers = GetComponentsInChildren<SpriteRenderer> ();
+
+		int order = 0;
+		foreach (SpriteRenderer r in renderers)
+		{
+			r.sortingOrder = order++;
+		}
+	}
+}
