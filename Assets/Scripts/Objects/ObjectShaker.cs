01bb1632cce3846528f3388f9ef8b40ef11de4b1
Michel Wacker <mw@gentletroll.com>
2015-01-25 00:07:03 +0100

    let objects shake when player starts getting mad

---
+++ Assets/Scripts/Objects/ObjectShaker.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectShaker : MonoBehaviour
+{	
+	private Vector3 p;
+	
+	void Update()
+	{
+		float rand = (Player.currentMadness - 3f);
+		rand = (rand < 0) ? 0 : rand;
+		rand *= 0.01f;
+		transform.position = p + new Vector3 (Random.Range(-rand,rand), Random.Range(-rand,rand), 0f);
+	}
+	
+	protected void Awake()
+	{
+		p = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+	}
+}
