27b53fa8c76a01fef22b6b1bf0f68ee186218645
Michel Wacker <mw@gentletroll.com>
2015-01-24 17:13:23 +0100

    add scenemanager script

---
+++ Assets/Scripts/Scene/SceneManager.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneManager : MonoBehaviour
+{
+
+	public InteractionMenuManager interactionMenuManager;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	void OnEnable()
+	{
+		Player.WalkBegin += HandleWalkBegin;
+		Player.WalkComplete += HandleWalkComplete;
+	}
+
+	void OnDisable()
+	{
+		Player.WalkBegin -= HandleWalkBegin;
+		Player.WalkComplete -= HandleWalkComplete;
+	}
+	void HandleWalkBegin (GameObject gameObject)
+	{
+		Debug.Log ("HandleWalkBegin");
+		HideInteractionMenu();
+	}
+	void HandleWalkComplete (GameObject gameObject)
+	{
+		Debug.Log ("HandleWalkComplete");
+		ShowInteractionMenu(gameObject.transform.position);
+	}
+
+	public void HandleUseDown()
+	{
+		Debug.Log ("HandleUseDown");
+		HideInteractionMenu ();
+
+	}
+	public void HandleBreakDown()
+	{
+		Debug.Log ("HandleBreakDown");
+		HideInteractionMenu ();
+	}
+
+	private void ShowInteractionMenu(Vector3 position)
+	{
+		interactionMenuManager.ShowMenu (position);
+	}
+
+	private void HideInteractionMenu()
+	{
+		interactionMenuManager.HideMenu ();
+	}
+}
