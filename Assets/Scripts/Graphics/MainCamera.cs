110523cb436d1178bb228fdfd877f29cc6df856c
joserodolfo-freitas <joserodolfo.freitas@gmail.com>
2015-01-25 14:21:45 +0100

    shader fix try

--- Assets/Scripts/Graphics/MainCamera.cs
+++ Assets/Scripts/Graphics/MainCamera.cs
@@ -6,5 +6,8 @@ public class MainCamera : MonoBehaviour {
 	public Material material;
 	public RenderTexture intermediateRT;
 
+	void OnRenderImage (RenderTexture source, RenderTexture destination){
 
+		Graphics.Blit(source, destination, material);
+	}
 }
