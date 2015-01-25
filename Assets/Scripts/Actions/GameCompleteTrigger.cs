1a049204211d969c5540a75e3b44238f856209d6
Michel Wacker <mw@gentletroll.com>
2015-01-25 13:24:59 +0100

    add game complete trigger

---
+++ Assets/Scripts/Actions/GameCompleteTrigger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameCompleteTrigger : MonoBehaviour, ITriggerAction
+{
+	public int madness = 0;
+	#region ITriggerAction implementation
+	public void Action ()
+	{
+		Debug.Log ("GAME COMPLETE!");
+		Player.SetMadness (madness);
+
+		SceneManager.GameComplete();
+	}
+	#endregion
+	
+}
