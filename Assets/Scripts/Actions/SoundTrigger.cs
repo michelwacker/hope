25911f7ae7411f7d740fbe5a3663f48d4e03fe91
Michel Wacker <mw@gentletroll.com>
2015-01-25 15:04:22 +0100

    add first sounds

---
+++ Assets/Scripts/Actions/SoundTrigger.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundTrigger : MonoBehaviour, ITriggerAction
+{
+	public AudioClip clip;
+	private AudioSource source;
+
+	void Awake()
+	{
+		source = GetComponent<AudioSource> ();
+	}
+	#region ITriggerAction implementation
+
+	public void Action ()
+	{
+		if (source != null && clip != null)
+		{
+//			source.clip = clip;
+			source.PlayOneShot(clip);
+		}
+	}
+
+	#endregion
+
+
+}
