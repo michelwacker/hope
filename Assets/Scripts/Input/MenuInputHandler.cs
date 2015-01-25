138fe75cf5addf1ad75ccdca31c106d603cf7838
joserodolfo-freitas <joserodolfo.freitas@gmail.com>
2015-01-25 13:19:22 +0100

    Fixed menu and editted build settings

--- Assets/Scripts/Input/MenuInputHandler.cs
+++ Assets/Scripts/Input/MenuInputHandler.cs
@@ -3,19 +3,9 @@ using System.Collections;
 
 public class MenuInputHandler : MonoBehaviour {
 
-	#region IInputHandler implementation
-	public void HandleInputDown (Collider2D target, int fingerId = -1)
-	{
-		Debug.Log("menu!!!");
-		Application.LoadLevel ("Scene1");
+	void Update() {
+		if (Input.GetMouseButtonDown(0)) {
+			Application.LoadLevel("Scene1");
+		}
 	}
-	public void HandleInputUpdate (Vector2 position, int fingerId = -1)
-	{
-		// empty
-	}
-	public void HandleInputUp (int fingerId = -1)
-	{
-		// empty
-	}
-	#endregion
 }
