6f2b9051d2bdb92ea8f73f5fff7ecab8a41c0df3
joserodolfo-freitas <joserodolfo.freitas@gmail.com>
2015-01-25 14:15:48 +0100

    build without shader

--- Assets/Scripts/Graphics/MainCamera.cs
+++ Assets/Scripts/Graphics/MainCamera.cs
@@ -6,8 +6,5 @@ public class MainCamera : MonoBehaviour {
 	public Material material;
 	public RenderTexture intermediateRT;
 
-	void OnRenderImage (RenderTexture source, RenderTexture destination){
-		Graphics.Blit(source, intermediateRT, material);
-		Graphics.Blit(intermediateRT, destination);
-	}
+
 }
