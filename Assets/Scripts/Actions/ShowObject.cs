a51757a458bcd476cab7216164b3f20907c3cf53
Michel Wacker <mw@gentletroll.com>
2015-01-25 01:52:16 +0100

    Add ShowObject action

---
+++ Assets/Scripts/Actions/ShowObject.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowObject : MonoBehaviour, ITriggerAction
+{
+	public enum ShowType
+	{
+		Hide,
+		Show
+	}
+	public ShowType action = ShowType.Show;
+	public GameObject[] targets;
+
+	void Awake()
+	{
+		if (targets.Length == 0)
+		{
+			Debug.LogWarning("No targets set in ShowObject action of " + name);
+		}
+	}
+
+	#region ITriggerAction implementation
+	public void Action ()
+	{
+		if (targets.Length > 0)
+		{
+			foreach(GameObject t in targets)
+			{
+				t.SetActive(action == ShowType.Show);
+			}
+		}
+	}
+	#endregion
+}
