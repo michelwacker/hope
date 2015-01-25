214d15588f4f4662afa2e93b9a0e71478cf3bf85
joserodolfo-freitas <joserodolfo.freitas@gmail.com>
2015-01-25 13:08:52 +0100

    Menu Input -not working-

---
+++ Assets/Scripts/Input/MenuInputHandler.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuInputHandler : MonoBehaviour {
+
+	#region IInputHandler implementation
+	public void HandleInputDown (Collider2D target, int fingerId = -1)
+	{
+		Debug.Log("menu!!!");
+		Application.LoadLevel ("Scene1");
+	}
+	public void HandleInputUpdate (Vector2 position, int fingerId = -1)
+	{
+		// empty
+	}
+	public void HandleInputUp (int fingerId = -1)
+	{
+		// empty
+	}
+	#endregion
+}
