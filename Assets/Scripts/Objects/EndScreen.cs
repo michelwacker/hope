0ed6419eca2fcc8b483a56b0dc4c732fa3e35867
joserodolfo-freitas <joserodolfo.freitas@gmail.com>
2015-01-25 14:02:17 +0100

    End

--- Assets/Scripts/Objects/EndScreen.cs
+++ Assets/Scripts/Objects/EndScreen.cs
@@ -2,19 +2,28 @@
 using System.Collections;
 
 public class EndScreen : MonoBehaviour {
-	SpriteRenderer sprite;
 	// Use this for initialization
+
+	public void Awake()
+	{
+		if (gameObject.collider2D != null)
+			gameObject.collider2D.enabled = false;
+		
+		Color c = gameObject.transform.renderer.material.color;
+		gameObject.transform.renderer.material.color = new Color (c.r,c.g,c.b, 0f);
+	}
+
 	void Start () {
-		sprite = GetComponent<SpriteRenderer>();
+		//sprite = GetComponent<SpriteRenderer>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if(SceneManager.isGameOver()) {
-			iTween.FadeFrom (gameObject, iTween.Hash (
-				"alpha", 1f,
-				"amount", 0f,
-				"time", 3f,
+			iTween.FadeTo (gameObject, iTween.Hash (
+				"alpha", 0f,
+				"amount", 1f,
+				"time", 5f,
 				"easetype", "easeInOutSine",
 				"looptype", "pingPong"
 				));
