dedea7daee672f883366c273ea5d14aa4ee6cf7e
Michel Wacker <mw@gentletroll.com>
2015-01-25 13:24:43 +0100

    scripts triggering the mouse

---
+++ Assets/Scripts/Actions/MouseMover.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseMover : MonoBehaviour, ITriggerAction
+{
+	public Transform foodAnchor;
+
+	private Vector3 initPos;
+	private Animator anim;
+
+	void Awake()
+	{
+		Vector3 p = transform.position;
+
+		initPos = new Vector3 (p.x, p.y, p.z);
+
+		anim = GetComponent<Animator> ();
+	}
+
+	#region ITriggerAction implementation
+	public void Action ()
+	{
+		StartCoroutine (WaitForRun());
+	}
+	#endregion
+	IEnumerator WaitForRun()
+	{
+		yield return new WaitForSeconds (2f);
+		Run ();
+	}
+	void Run()
+	{
+		anim.SetBool("Feed", false);
+		
+		gameObject.transform.position = new Vector3 (initPos.x, initPos.y, initPos.z);
+		gameObject.SetActive (true);
+		
+		iTween.MoveTo(gameObject, iTween.Hash(
+			"position", foodAnchor.transform.position,
+			"onComplete", "OnRunComplete",
+			"easeType", "linear"));
+	}
+	void OnRunComplete()
+	{
+		if (anim != null)
+		{
+			anim.SetBool("Feed", true);
+		}
+	}
+}
---
+++ Assets/Scripts/Actions/MouseTrigger.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseTrigger : MonoBehaviour, ITriggerAction
+{
+	
+	public GameObject mouse;
+
+	#region ITriggerAction implementation
+	public void Action ()
+	{
+		if (mouse != null)
+		{
+			MouseMover m = mouse.GetComponent<MouseMover>();
+			if (m != null)
+			{
+				m.Action();
+			}
+		}
+	}
+	#endregion
+}
