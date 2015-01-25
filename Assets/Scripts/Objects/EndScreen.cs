05c10af1b4a0a75bc07a3e307977cfdf58d3eac4
joserodolfo-freitas <joserodolfo.freitas@gmail.com>
2015-01-25 13:48:35 +0100

    EndScreen prot1

---
+++ Assets/Scripts/Objects/EndScreen.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndScreen : MonoBehaviour {
+	SpriteRenderer sprite;
+	// Use this for initialization
+	void Start () {
+		sprite = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(SceneManager.isGameOver()) {
+			Color c = sprite.color;
+			c.a = 1;
+			sprite.color = c;
+		}
+	
+	}
+}
